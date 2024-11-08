37a7f23acf21e6263479be3259b87e320d80ce32
degel01459 <15-11661@usb.ve>
2024-11-08 14:03:43 -0400

    added black tool

--- fib.py
+++ fib.py
@@ -5,9 +5,10 @@ El número cero en la secuencia de Fibonacci es 0. El primer número es 1
 Los números negativos no son aceptados
 """
 
+
 def fibonacci(position):
-  if(position < 0):
-    raise ValueError("Invalid input")
-  if(position == 1 or position == 2):
-    return 1
-  return fibonacci(position - 1) + fibonacci(position - 2)
+    if position < 0:
+        raise ValueError("Invalid input")
+    if position == 1 or position == 2:
+        return 1
+    return fibonacci(position - 1) + fibonacci(position - 2)
